2d3019251c2d70e3a94fd676ca71923b0cf3149f
longi <longi@192.168.38.19>
2018-01-17 16:34:19 +0900

    今度こそ最初の最初

---
+++ src/groupwork/Groupwork.java
@@ -0,0 +1,15 @@
+/*
+ * Java研修生9期グループワーク
+ * 仮想在庫システム
+ */
+package groupwork;
+
+/**
+ * [処理概要]
+ * @author  株式会社ワールドインテック SI事業部 福岡営業所 長島雅人
+ * @since   2018/00/17
+ * @version 1.0
+ */
+public class Groupwork {
+
+}
