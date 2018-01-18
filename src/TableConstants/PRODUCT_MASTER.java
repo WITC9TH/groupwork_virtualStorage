3ce417827f26944bf452b4d0d2fd84773ff05e2c
nagashimam <31643565+nagashimam@users.noreply.github.com>
2018-01-18 20:25:34 +0900

    クラスの位置を変える

---
+++ src/TableConstants/PRODUCT_MASTER.java
@@ -0,0 +1,37 @@
+package TableConstants;
+
+import java.util.List;
+
+public enum PRODUCT_MASTER implements TABLES {
+    P_ID,
+    C_ID,
+    P_NAME,
+    BUY_PRICE,
+    SELL_PRICE,
+    CREATE_DATE,
+    CREATE_USER;
+ 
+    public static String getTableName(){
+        return TABLES.getTableName(PRODUCT_MASTER.class);
+    }
+    
+    public static String getMainKey() {
+        return P_ID.getColumn();
+    }
+    
+    public static String getForeingKey() {
+        return C_ID.getColumn();
+    }
+    
+    public static String getAllColumnsWithComma(){
+        return TABLES.getAllColumnsWithComma(PRODUCT_MASTER.class);
+    }
+    
+    public static List<String> getAllColumnsAsStringList(){
+        return TABLES.getAllColumnsAsStringList(PRODUCT_MASTER.class);
+    }
+
+    public String getColumn() {
+        return this.name();
+    }
+}
