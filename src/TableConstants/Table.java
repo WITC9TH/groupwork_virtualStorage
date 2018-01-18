0c595c97756e4ac17afc0a8442f9bc995f66ea17
longi <longi@192.168.38.19>
2018-01-18 14:33:04 +0900

    テーブル定数を扱うクラスで共通する処理を定義したインターフェースです

---
+++ src/TableConstants/Table.java
@@ -0,0 +1,75 @@
+/*
+ * Java研修生9期グループワーク
+ * 仮想在庫システム
+ */
+package TableConstants;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * [処理概要] テーブル関連の列挙型に共通する処理を定義する
+ *
+ * @author 株式会社ワールドインテック SI事業部 福岡営業所 長島雅人
+ * @since 2018年01月18日
+ * @version 1.0
+ */
+public interface Table {
+
+    static <T> String getTableName(final Class<T> clazz) {
+        String tableName = null;
+
+        if (clazz.isEnum()) {
+            tableName = clazz.getSimpleName();
+        } else {
+            throwIllegalArgumentException();
+        }
+
+        return tableName;
+    }
+
+    static <T> String getAllColumnsWithComma(final Class<T> clazz) {
+        StringBuilder sb = new StringBuilder();
+
+        if (clazz.isEnum()) {
+            Field[] fields = clazz.getFields();
+            int commaNum = fields.length - 1;
+
+            try {
+                for (int i = 0; i < commaNum; i++) {
+                    Field field = fields[i];
+                    sb.append(field.get(field));
+                    sb.append(",");
+                }
+                sb.append(fields[commaNum].get(fields[commaNum]));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+
+    static <T> List<String> getAllColumnsAsStringList(final Class<T> clazz) {
+        List<String> allColumns = new ArrayList<>();
+
+        if (clazz.isEnum()) {
+            for (Field field : clazz.getFields()) {
+                allColumns.add(field.getName());
+            }
+        }else{
+            throwIllegalArgumentException();
+        }
+
+        return allColumns;
+    }
+
+    static void throwIllegalArgumentException() {
+        try {
+            throw new IllegalArgumentException("列挙型のクラスインスタンス以外を渡さないでください");
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
