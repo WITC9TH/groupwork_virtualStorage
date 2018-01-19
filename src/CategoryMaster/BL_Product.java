be8cc47813d2f552eb7cd9fffd2f7eb3a1280d4f
nagashimam <31643565+nagashimam@users.noreply.github.com>
2018-01-19 11:46:48 +0900

    CategoryMasterパッケージにビジネスロジッククラスを追加しました。

---
+++ src/CategoryMaster/BL_Product.java
@@ -0,0 +1,16 @@
+/*
+ * Java研修生9期グループワーク
+ * 仮想在庫システム
+ */
+package CategoryMaster;
+
+/**
+ * [処理概要]
+ * 商品マスター画面における基幹処理を行う
+ * @author  株式会社ワールドインテック SI事業部 福岡営業所 長島雅人
+ * @since   2018年01月19日
+ * @version 1.0
+ */
+public class BL_Product {
+
+}
