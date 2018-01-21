2a7be6146b2337462a0bf576b5aef24420f5c899
nagashimam <31643565+nagashimam@users.noreply.github.com>
2018-01-21 14:20:43 +0900

    DTOを作成しました

---
+++ src/CategoryMaster/DTO_Category.java
@@ -0,0 +1,16 @@
+/*
+ * Java研修生9期グループワーク
+ * 仮想在庫システム
+ */
+package CategoryMaster;
+
+/**
+ * [処理概要]
+ * カテゴリマスタ画面で入力されたデータを保持する
+ * @author  株式会社ワールドインテック SI事業部 福岡営業所 長島雅人
+ * @since   2018年01月21日
+ * @version 1.0
+ */
+public class DTO_Category {
+
+}
